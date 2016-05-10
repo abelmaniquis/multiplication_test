386ca0a2f6d1f5c86069e18a9f9a13e8b42fa414
abelmaniquis <abelmaniquis@gmail.com>
2016-05-10 18:30:46 +0000

    testing variables

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 "use strict"
 
-
+var super1 = 0;
+var super2 = 0;
 $("#btn1").click(function(){
     state_problem();
+    evaluate(super1,super2,84); //test
+    alert(ans);
 });
 
 
@@ -10,6 +13,10 @@ function state_problem(){
     var tries = 0;
     var num1 = Math.floor(Math.random() * (13 - 1) + 1);
     var num2 = Math.floor(Math.random() * (13 - 1) + 1);
+    
+    super1 = num1;
+    super2 = num2;
+    
     $("#problem").empty();
     $("#userAnswer").empty();
     $("#problem").append(num1 + " X " + num2);
@@ -25,3 +32,6 @@ function evaluate(factor1,factor2,answer){
     }
     
 };
+
+//need to figure out how to transfer num1 and num2 into
+//evaluate function without using global scope
